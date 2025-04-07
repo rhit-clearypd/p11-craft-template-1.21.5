fcff0bc5f77e6b6812c7b6b6aca27aa9b9d9ade8
rhit-clearypd <clearypd@rose-hulman.edu>
2025-04-07 11:07:57 -0400

    FIXED RUBY ITEM!! Needed to add register keys in item construction, then add a new directory for "items" under assets with another json for each item

--- src/main/java/net/pc2/p11craft/item/ModItems.java
+++ src/main/java/net/pc2/p11craft/item/ModItems.java
@@ -3,6 +3,8 @@ package net.pc2.p11craft.item;
 // p11: helper class to register items, so minecraft knows we made new items :)
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.pc2.p11craft.P11Craft;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,7 +13,10 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+
+    // p11: added ".registryKey...RegistryKeys.ITEM" after Item.Settings() based on comments
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings().registryKey(RegistryKey.of(
+            RegistryKeys.ITEM, Identifier.of(P11Craft.MOD_ID,"ruby")))));
 
 
     public static Item registerItem(String name, Item item) {
@@ -21,8 +26,6 @@ public class ModItems {
     public static void registerModItems() {
         P11Craft.LOGGER.info("Registering Mod Items for " + P11Craft.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
-            fabricItemGroupEntries.add(RUBY);
-        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> fabricItemGroupEntries.add(RUBY));
     }
 }
